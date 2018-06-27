d8c88db58a17f1a314e1f53691ca7793b248e948
Eagle <kromel@openmailbox.org>
2018-06-27 22:09:37 +0200

    Add files via upload

---
+++ proxy.py
@@ -0,0 +1,43 @@
+import requests
+import os
+
+def proxies():
+	os.system('cls' if os.name == 'nt' else 'clear')
+	print("""
+'              |    |    |                         '
+'             )_)  )_)  )_)                        '
+'            )___))___))___)\\                      '
+'           )____)____)_____)\\                    '
+'         _____|____|____|____\\\\__                '
+' ---------\\                   /---------          '
+'   ^^^^^ ^^^^^^^^^^^^^^^^^^^^^                    '
+'     ^^^^      ^^^^     ^^^    ^^                 '
+'          ^^^^      ^^^                           '
+'              Proxies checker                      '
+""")
+	file = input("Your proxies txt to check: ")
+	proxies = []
+	alive = []
+	url = "http://www.mon-ip.com/"
+	header = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1'}
+	with open(file, "r") as p:
+		proxies = [line.strip() for line in p]
+
+	for proxy in proxies:
+		start = 0
+		end = proxy.index(":")
+		st = proxy[start:end]
+		e = proxy[end+1:]
+		proxie = {"http":"{}:{}".format(st, e)}
+		r = requests.get(url, headers=header, proxies=proxie)
+		if st not in r.text:
+			print("Testing...")
+		else:
+			print("Testing...")
+			alive.append(proxy)
+	for prox in alive:
+		print("\nAlive list:")
+		print(prox)
+
+
+proxies()
